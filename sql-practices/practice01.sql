d94bc59e43de78aa4e78c8bd1194cfecaeeab039
yoncho <joyonghyeon13@gmail.com>
2023-08-29 16:07:17 +0900

    study - sql select

--- sql-practices/practice01.sql
+++ sql-practices/practice01.sql
@@ -8,7 +8,7 @@ select first_name from employees where emp_no='10944';
 -- 문제2. 
 -- 전체직원의 다음 정보를 조회하세요. 가장 -- 선임부터 출력이 되도록 하세요. 
 -- 출력은 이름, 성별,  입사일 순서이고 “이름”, “성별”, “입사일로 컬럼 이름을 대체해 보세요.
-select first_name as '이름', gender as '성별', hire_date as '입사일' from employees order by 
+select first_name as '이름', gender as '성별', hire_date as '입사일' from employees order by hire_date asc;
 
 -- 문제3.
 -- 여직원과 남직원은 각 각 몇 명이나 있나요?
@@ -22,19 +22,19 @@ select count(*) from salaries where to_date='9999-01-01';
 select count(*) from departments;
 -- 문제6.
 -- 현재 부서 매니저는 몇 명이나 있나요?(역임 -- 매너저는 제외)
--- select count(*) from departments where dept_manager='manager'; 
+select count(*) from dept_manager where to_date='9999-01-01'; 
 -- 문제7.
 -- 전체 부서를 출력하려고 합니다. 순서는 -- 부서이름이 긴 순서대로 출력해 보세요.
-select first_name, length(first_name) from employees;
+select dept_name from departments order by length(dept_name) desc;
 -- 문제8.
 -- 현재 급여가 120,000이상 받는 사원은 몇 명이나 -- 있습니까?
-
+select count(*) from salaries where to_date='9999-01-01' and salary >= '120000';
 -- 문제9.
 -- 어떤 직책들이 있나요? 중복 없이 이름이 긴 -- 순서대로 출력해 보세요.
- 
+select distinct title from titles order by length(title) desc;
 -- 문제10
 -- 현재 Enginner 직책의 사원은 총 몇 명입니까?
- 
+ select count(*) from titles where title like '%engineer' and to_date='9999-01-01';
 -- 문제11
 -- 사번이 13250(Zeydy)인 직원이 직책 변경 상황을 시간순으로 출력해보세요.
-select 
+select * from titles where emp_no='13250' order by from_date;
